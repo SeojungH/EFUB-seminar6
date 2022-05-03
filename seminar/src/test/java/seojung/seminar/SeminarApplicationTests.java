5740a217bfdb9a850d3a701c8971c24c9886ff5a
seojung <chseojung@naver.com>
2022-05-03 21:25:08 +0900

    Initial commit

---
+++ seminar/src/test/java/seojung/seminar/SeminarApplicationTests.java
@@ -0,0 +1,13 @@
+package seojung.seminar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeminarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
